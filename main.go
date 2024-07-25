45b443809cc6ea9a4dc6d209c98953b7eef2f94d
Clebson Moura <clebson.sh@gmail.com>
2024-07-25 20:57:39 -0300

    Create a simple web server with home handler

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+// Define a home handler function which writes a byte slice
+// containing "Hello from Snippetbox" as the response body.
+func home(w http.ResponseWriter, r *http.Request) {
+	// Check if the current request URL path exactly matches
+	// "/". If it doesn't, use the http.NotFound() function
+	// to send a 404 respponse to the client. Importantly,
+	// we then return form the handler. If we don't return
+	// the handler would keep executing and also write the
+	// "Hello from SnippetBox" message.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func main() {
+	// Use the http.NewServeMux() function to initialize a
+	// new servemux, then register the home function as the
+	// handlesr for the "/" URL pattern.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	// Use the http.ListenAndServe() function to start a new
+	// web server. We pass in two parameters: the TCP network
+	// address to listen on (in this case ":4000") and the
+	// servermux we just created. If http.ListenAndServe() return
+	// an error we use the log.Fatal() function to log the error
+	// message and exit.
+	// Note that any error returned by  http.ListenAndServe()
+	// is always non-nil.
+	log.Print("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+
+}
