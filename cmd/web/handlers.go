fdee5ccd3d29aa18234fc726ae7996e65e588ba5
Clebson Moura <clebson.sh@gmail.com>
2024-10-18 17:17:30 -0300

    creating validation helpers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,12 +5,11 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"strings"
-	"unicode/utf8"
 
 	"github.com/julienschmidt/httprouter"
 
 	"github.com/clebsonsh/snippetbox/internal/models"
+	"github.com/clebsonsh/snippetbox/internal/validator"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -61,10 +60,10 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 type snippetCreateForm struct {
-	FieldErrors map[string]string
-	Title       string
-	Content     string
-	Expires     int
+	validator.Validator
+	Title   string
+	Content string
+	Expires int
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
@@ -82,27 +81,17 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	}
 
 	form := snippetCreateForm{
-		Title:       r.PostForm.Get("title"),
-		Content:     r.PostForm.Get("content"),
-		Expires:     expires,
-		FieldErrors: map[string]string{},
+		Title:   r.PostForm.Get("title"),
+		Content: r.PostForm.Get("content"),
+		Expires: expires,
 	}
 
-	if strings.TrimSpace(form.Title) == "" {
-		form.FieldErrors["title"] = "This filed cannot be black"
-	} else if utf8.RuneCountInString(form.Title) > 100 {
-		form.FieldErrors["title"] = "This filed cannot be more than 100 characters long"
-	}
+	form.CheckField(validator.NotBlank(form.Title), "title", "This filed cannot be black")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This filed cannot be more than 100 characters long")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This filed cannot be black")
+	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This filed must equal 1, 7 or 365")
 
-	if strings.TrimSpace(form.Content) == "" {
-		form.FieldErrors["content"] = "This filed cannot be black"
-	}
-
-	if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
-		form.FieldErrors["expires"] = "This filed must equal 1, 7 or 365"
-	}
-
-	if len(form.FieldErrors) > 0 {
+	if !form.Valid() {
 		data := app.newTemplateData(r)
 		data.Form = form
 		app.render(w, http.StatusUnprocessableEntity, "create.tmpl", data)
