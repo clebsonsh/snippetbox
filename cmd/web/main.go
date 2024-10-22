dbee33f631287b238d4833d1b073a3d074feaf6e
Clebson Moura <clebson.sh@gmail.com>
2024-10-22 20:12:46 -0300

    using request context

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,7 +13,6 @@ import (
 	"github.com/alexedwards/scs/mysqlstore"
 	"github.com/alexedwards/scs/v2"
 	"github.com/go-playground/form/v4"
-	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/clebsonsh/snippetbox/internal/models"
 )
